52925a3f6cc7c36cd6d461c4276fc8de3d59f8bd
t.beck <t.beck1@outlook.com>
2023-04-28 03:32:11 -0700

    first js assignment

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+// console.log('Hello Wolrd!');
+
+// const myVariable = 'Hello World';
+// console.log(myVariable);
+
+// const myVariable = 5;
+// console.log(myVariable);
+
+// let myVariable;
+// console.log(myVariable);
